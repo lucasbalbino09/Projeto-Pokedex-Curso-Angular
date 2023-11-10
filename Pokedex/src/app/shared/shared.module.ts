7f34569426b95bc6eff3b9cd1fe1348fa6b35fe3
lucas.balbino <balbino2409@gmail.com>
2023-11-10 08:13:09 -0300

    subindo continuação dos estudos pokedex

--- Pokedex/src/app/shared/shared.module.ts
+++ Pokedex/src/app/shared/shared.module.ts
@@ -4,17 +4,20 @@ import { CommonModule } from '@angular/common';
 //Components
 import { PokeHeaderComponent } from './poke-header/poke-header.component';
 import { PokeSearchComponent } from './poke-search/poke-search.component';
+import { PokeListComponent } from './poke-list/poke-list.component';
 
 
 
 @NgModule({
   declarations: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   exports: [
     PokeHeaderComponent,
-    PokeSearchComponent
+    PokeSearchComponent,
+    PokeListComponent
   ],
   imports: [
     CommonModule
